7212ac50f4fff0b4e56a7ba2cb2693730f263a4f
chiefJurist <anthonynnanna004@gmail.com>
2023-08-18 05:00:15 +0100

    Starting A new game

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,31 @@
 <template>
     <h1>Tony Reaction Timer</h1>
+    <button @click="start" :disabled="isplaying">play</button>
+    <!--Above we are setting the play button to be disabled while we play-->
+    <!--Below we set the delay for the block appearance-->
+    <Block v-if="isplaying" :delay="delay"/>
 </template>
 
+
 <script>
+    import Block from "./components/Block.vue"
+
     export default{
         name: 'App',
-        components: {  }
+        components: { Block },
+        data(){
+            return{
+                isplaying: false,
+                delay: null
+            }
+        },
+        methods: {
+            start() {
+                this.isplaying = true
+                this.delay = 2000 + Math.random() * 5000
+                console.log(this.delay)
+            }
+        }
     }
 </script>
 
@@ -15,7 +35,7 @@
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
-        color: #2c3e50;
+        color: #444;
         margin-top: 60px;
     }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="block">
+        click me
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['delay']
+    }
+</script>
+
+<style>
+    .block{
+        width: 400px;
+        border-radius: 20px;
+        background: #0faf87;
+        color: white;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
